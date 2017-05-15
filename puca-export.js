20aac09f0cfd4b2f8c3e1a1b349869d6ee2e1543
Michael Baranov <tdbear@users.noreply.github.com>
2017-05-15 19:16:51 -0400

    Update puca-export.js

--- puca-export.js
+++ puca-export.js
@@ -1,3 +1,22 @@
 (function($) {
-  alert('hello!');
+  
+  alert('Ready to export your haves');
+  
+  var export = function(){
+    alert('exporting');
+  };
+  
+  var expandMore = function() {
+    $more = $(":contains('LOAD MORE')");
+    if($more.length == 0) {
+      export();
+      return;
+    }
+    $more.click();
+    setTimeout(this, 3000);
+  };
+  
+  expandMore();
+  
+  
 })($);
