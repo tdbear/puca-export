65a73b79abfd2d3267c7d05db138b294c9f6ca52
Michael Baranov <tdbear@users.noreply.github.com>
2017-05-15 20:29:32 -0400

    Update puca-export.js

--- puca-export.js
+++ puca-export.js
@@ -1,9 +1,26 @@
 (function($) {
   
-  alert('Ready to export your haves');
+  alert('Ready to export.\nThis includes loading all your haves which may take some time.');
   
-  var doExport = function(){
+  var doExport = function() {
     console.log('exporting');
+    
+    var $cards = $('.item.clear').find('a[data-card_id]');
+    
+    for (var i=1; i<$cards.length; i++) {
+        var $card = $($cards[i]);
+      
+        var card = {
+          name: $card.find('.set select option:selected').text(),
+          isTradable: $card.find('.trade .btn-tab').hasClass('active-button-2'),
+          isFoil: $card.find('.foil select option:selected').val() == 1,
+          language: $card.find('.language select option:selected').text(),
+          condition: $card.find('.condition select option:selected').text()
+        };
+      
+        console.log(card);
+    }
+    
   };
   
   var expandMore = function() {
@@ -13,6 +30,7 @@
       return;
     }
     
+    console.log('expanding page');
     var evt = new Event("click", {"bubbles":true, "cancelable":true});
     $more[0].dispatchEvent(evt);
     
