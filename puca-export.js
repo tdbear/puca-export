a8f4df2545b9834fa9d889c78f7442056d292ddf
Michael Baranov <tdbear@users.noreply.github.com>
2017-05-15 20:06:10 -0400

    Update puca-export.js

--- puca-export.js
+++ puca-export.js
@@ -8,11 +8,14 @@
   
   var expandMore = function() {
     var $more = $("a:contains('LOAD MORE')");
-    if($more.length == 0) {
+    if($more.length == 1) {
       doExport();
       return;
     }
-    $more.click();
+    
+    var evt = new Event("click", {"bubbles":true, "cancelable":true});
+    $more[0].dispatchEvent(evt);
+    
     setTimeout(expandMore, 3000);
   };
   
