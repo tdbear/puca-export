fd59f1ec2b39743a2c64daeb0edf2afa09f7b850
Michael Baranov <tdbear@users.noreply.github.com>
2017-05-15 21:16:35 -0400

    Update puca-export.js

--- puca-export.js
+++ puca-export.js
@@ -24,8 +24,18 @@
     }
     
     var groups = _.groupBy(cards, function(c){ return JSON.stringify(c); });
-    console.log(countedCards);
+    console.log(groups);
     
+    var csv = ['Count', 'Name', 'Expansion', 'Condition', 'Language', 'Foil', 'PucaID', 'Status'].join('\t');
+    for (var i=0; i<groups.length; i++){
+      var group = groups[i];
+      var card = group[0];
+      var quantity = group.length;
+      var row = [quantity, card.cardName, card.setName, card.condition, card.language, card.isFoil, '', card.isTradable];
+      csv = csv + '\n' + row.join('\t');
+    }
+    
+    window.open('data:text/csv;charset=utf-8,' + escape(csv));
     
   };
   
